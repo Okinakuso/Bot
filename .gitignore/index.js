7d98b89ee4e00ee4f029881235e59b021a8dc8b3
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2018-04-19 19:35:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("Kurumie, ?help");
+    bot.user.setGame("?help");
     console.log("Le bot est bien connecte")
 });
 
