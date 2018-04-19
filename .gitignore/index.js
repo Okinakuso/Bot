1277961aabe4b1ea0dd1a12906287493e5f12f8f
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2018-04-19 19:20:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,3 +15,24 @@ bot.on("message", message => {
   }
 
 });
+
+bot.on("message", message => {
+    if (message.content === "?nitroz") {
+    message.channel.sendMessage("Un gros con qui dit tout le temps <<comme tu veut>>");
+  }
+
+});
+
+bot.on("message", message => {
+    if (message.content === "?cora") {
+    message.channel.sendMessage("La plus belle femme du monde et aussi futur femme de okina");
+  }
+
+});
+
+bot.on("message", message => {
+    if (message.content === "?okina") {
+    message.channel.sendMessage("Sombre en***er qui ressent le besoin d etre superieur au autres et est aussi le Fondateur");
+  }
+
+});
