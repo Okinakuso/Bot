3347ad228ef09f111f031145d80985bd44693e18
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2018-04-20 16:29:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("?help");
+    bot.user.setGame("-help");
     console.log("Le bot est bien connecte")
 });
 
@@ -11,7 +11,7 @@ bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
-  if (message.content === "?avatar") {
+  if (message.content === "-avatar") {
     message.channel.sendMessage(message.author.avatarURL);
   }
 });
@@ -19,49 +19,49 @@ bot.on('message', message => {
 
 
 bot.on("message", message => {
-    if (message.content === "?ping") {
+    if (message.content === "-ping") {
     message.channel.sendMessage("tu as cru que j allais dire pong ou quoi fils de p**e");
   }
 
 });
 
 bot.on("message", message => {
-    if (message.content === "?ching") {
+    if (message.content === "-ching") {
     message.channel.sendMessage("tu es raciste !");
   }
 
 });
 
 bot.on("message", message => {
-    if (message.content === "?nitroz") {
+    if (message.content === "-nitroz") {
     message.channel.sendMessage("Un gros con qui dit tout le temps <<comme tu veux>>");
   }
 
 });
 
 bot.on("message", message => {
-    if (message.content === "?cora") {
+    if (message.content === "-cora") {
     message.channel.sendMessage("La plus belle femme du monde et qui est aussi la futur femme de okina");
   }
 
 });
 
 bot.on("message", message => {
-    if (message.content === "?okina") {
+    if (message.content === "-okina") {
     message.channel.sendMessage("Sombre en***er qui ressent le besoin d etre superieur aux autres et qui est aussi le Fondateur des serveurs");
   }
 
 });
 
 bot.on("message", message => {
-    if (message.content === "?spara") {
+    if (message.content === "-spara") {
     message.channel.sendMessage("Rien a dire sur ce co***rd de flemmard...");
   }
 
 });
 
 bot.on("message", message => {
-    if (message.content === "?ayu") {
+    if (message.content === "-ayu") {
     message.channel.sendMessage("Quoi ?? elle est encore ici elle ?");
   }
 
@@ -70,15 +70,40 @@ bot.on("message", message => {
 
 
 bot.on("message", message => {
-  if (message.content === "?help") {
+  if (message.content === "-help") {
     var help_embed = new Discord.RichEmbed()
       .setColor("#B638FF")
-      .addField("Les commandes du bot !" ,  "Voici le menu ?help")
-      .addField("commandes p.1" , "?okina   ?cora")
-      .addField("commandes p.2" , "?nitroz   ?spara")
-      .addField("commandes p.3" , "?ayu      ?avatar")
+      .addField("Les commandes du bot !" ,  "Voici le menu -help")
+      .addField("commandes p.1" , "-okina   -cora")
+      .addField("commandes p.2" , "-nitroz   -spara")
+      .addField("commandes p.3" , "-ayu      -avatar")
+      .addField("commandes p.4" , "-site      -forum")
     message.channel.sendEmbed(help_embed);
     console.log("commande Help demandée !");
+
+  }
+
+});
+
+bot.on("message", message => {
+  if (message.content === "-site") {
+    var help_embed = new Discord.RichEmbed()
+      .setColor("#B638FF")
+      .url("Le lien du site"  ,  "https://fusionnetwork.online/")
+    message.channel.sendEmbed(help_embed);
+    console.log("commande site demandée !");
+
+  }
+
+});
+
+bot.on("message", message => {
+  if (message.content === "-forum") {
+    var help_embed = new Discord.RichEmbed()
+      .setColor("#B638FF")
+      .url("Le lien du forum"  ,  "https://fusionnetwork.online/forum/index.php?forums/")
+    message.channel.sendEmbed(help_embed);
+    console.log("commande forum demandée !");
 
   }
 
