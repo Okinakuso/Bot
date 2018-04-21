a3e3b65d08b758cd50fd50c5c25f5a27e8a4a0e1
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2018-04-21 11:31:59 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,87 +10,67 @@ bot.on("ready", function() {
 
 bot.login(process.env.TOKEN);
 
+function random(min, max) {
+  min = Math.ceil(0);
+  max = Math.floor(5);
+  randnum = Math.floor(Math.random() * (max -min +1) + min);
+}
 
 bot.on('message', message => {
   if (message.content === "-avatar") {
     message.channel.sendMessage(message.author.avatarURL);
   }
-});
 
 
-
-bot.on("message", message => {
-    if (message.content === "-ping") {
+ if (message.content === "-ping") {
     message.channel.sendMessage("tu as cru que j allais dire pong ou quoi fils de p**e");
   }
 
-});
 
-bot.on("message", message => {
-    if (message.content === "-ching") {
+ if (message.content === "-ching") {
     message.channel.sendMessage("tu es raciste !");
   }
 
-});
 
-bot.on("message", message => {
-    if (message.content === "-nitroz") {
+ if (message.content === "-nitroz") {
     message.channel.sendMessage("Un gros con qui dit tout le temps <<comme tu veux>>");
   }
 
-});
 
-bot.on("message", message => {
-    if (message.content === "-cora") {
+
+  if (message.content === "-cora") {
     message.channel.sendMessage("La plus belle femme du monde et qui est aussi la futur femme de okina");
   }
 
-});
 
-bot.on("message", message => {
-    if (message.content === "-okina") {
+  if (message.content === "-okina") {
     message.channel.sendMessage("Sombre en***er qui ressent le besoin d etre superieur aux autres et qui est aussi le Fondateur des serveurs");
   }
 
-});
 
-bot.on("message", message => {
-    if (message.content === "-spara") {
+  if (message.content === "-spara") {
     message.channel.sendMessage("Rien a dire sur ce co***rd de flemmard...");
   }
 
-});
 
-bot.on("message", message => {
-    if (message.content === "-ayu") {
+
+  if (message.content === "-ayu") {
     message.channel.sendMessage("Quoi ?? elle est encore ici elle ?");
   }
 
-});
 
-
-
-bot.on("message", message => {
   if (message.content === "-site") {
     message.channel.sendMessage("https://fusionnetwork.online/");
 
   }
 
-});
-
-bot.on("message", message => {
   if (message.content === "-forum") {
     message.channel.sendMessage("https://fusionnetwork.online/forum/index.php?forums/");
 
   }
 
-});
-
-bot.on("message", message => {
-    if (message.content === "-anime"){
-      random();
-
-    
+  if (message.content === "-anime"){
+      random();   
 
        if (randnum == 1){
            message.reply("reponse 1");
@@ -124,11 +104,6 @@ bot.on("message", message => {
   }
 
 
-
-}); 
-
-
-bot.on("message", message => {
   if (message.content === "-help") {
     var help_embed = new Discord.RichEmbed()
       .setColor("#B638FF")
@@ -140,9 +115,7 @@ bot.on("message", message => {
 
   }
 
-});
 
-bot.on("message", message => {
   if (message.content === "-help2") {
     var help_embed = new Discord.RichEmbed()
       .setColor("#B638FF")
@@ -150,12 +123,9 @@ bot.on("message", message => {
     message.channel.sendEmbed(help_embed);
     console.log("commande Help1 demandée !");
       
-        }
+  }
 
-});
-    
 
-bot.on("message", message => {
   if (message.content === "-help1") {
     var help_embed = new Discord.RichEmbed()
       .setColor("#B638FF")
@@ -163,13 +133,7 @@ bot.on("message", message => {
     message.channel.sendEmbed(help_embed);
     console.log("commande Help2 demandée !");
       
-        }
+  }
+
 
 });
-
-
-function random(min, max) {
-  min = Math.ceil(0);
-  max = Math.floor(5);
-  randnum = Math.floor(Math.random() * (max -min +1) + min);
-}
