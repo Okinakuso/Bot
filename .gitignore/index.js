cd6c53dd5d72b55931d2c75c1f45249322a42633
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2018-05-08 15:35:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,7 @@ bot.login(process.env.TOKEN);
 
 function random(min, max) {
   min = Math.ceil(1);
-  max = Math.floor(6);
+  max = Math.floor(5);
   randnum = Math.floor(Math.random() * (max -min +1) + min);
 }
 
