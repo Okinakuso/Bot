d23f6172b138b6ecc3b4fce367fe6a6bf3eacbf1
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2019-04-24 11:13:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -28,27 +28,38 @@ bot.on('message', message => {
     message.channel.sendMessage("t as cru que j allais dire pong ou quoi fils de pute");
   }
 
-
  if (message.content === ".ching") {
     message.channel.sendMessage("tu es raciste !");
-  }
-
+  }    
+    
+  if (message.content === ".anime") {
+    message.channel.sendMessage("https://www.randomanime.org/list/?l=703869fb&view=single&lang=any");
+  }   
+    
   if (message.content === ".pute") {
     message.channel.sendMessage("INVOCATION !");
     message.channel.sendMessage("http://image.noelshack.com/fichiers/2019/05/7/1549220487-dyvp0n-xqaajsne.jpg");
     message.channel.sendMessage(message.author.avatarURL);
   }
 
+  if (message.content === ".loli") {
+    message.channel.sendMessage("https://image.noelshack.com/fichiers/2019/17/1/1555964709-tenor.gif");
+  }
+    
+    
+    
+    
+    
+  if (message.content === ".yami") {
+    message.channel.sendMessage("Un homme qui ressent le besoin d etre superieur aux autres.");
+    message.channel.sendMessage("https://image.noelshack.com/fichiers/2019/17/1/1555965169-tumblr-pew9fkpgof1xqsds7o2-500.gif");
+  }
+    
   if (message.content === ".cora") {
     message.channel.sendMessage("La plus belle femme du monde et qui est aussi la futur femme de Yami");
     message.channel.sendMessage("https://media.giphy.com/media/bm2O3nXTcKJeU/giphy.gif");
   }
     
-  if (message.content === ".louhan") {
-    message.channel.sendMessage("Prenez garde à son futur 28M dps!");
-    message.channel.sendMessage("https://cdn.discordapp.com/attachments/568118396676341800/570343991292198932/20190423_222312.gif");
-  }
-
   if (message.content === ".illya") {
     message.channel.sendMessage("Le sac a foutre supreme mais aussi notre GM");
     message.channel.sendMessage("https://media1.tenor.com/images/1e0593d9bf7b27cde0be27007d00887e/tenor.gif?itemid=5531465");
@@ -59,19 +70,19 @@ bot.on('message', message => {
     message.channel.sendMessage("https://image.noelshack.com/fichiers/2019/17/1/1555964641-9aqn.gif");
   } 
     
+  if (message.content === ".golden") {
+    message.channel.sendMessage("Wallah c'est une victime");
+    message.channel.sendMessage("https://media.giphy.com/media/pPQyXGxwx272E/giphy.gif");
+  } 
+    
+  if (message.content === ".louhan") {
+    message.channel.sendMessage("Prenez garde à son futur 28M dps!");
+    message.channel.sendMessage("https://cdn.discordapp.com/attachments/568118396676341800/570343991292198932/20190423_222312.gif");
+  }
+    
   if (message.content === ".tezer") {
     message.channel.sendMessage("Paix et amour sont les mots qui le définissent!");
     message.channel.sendMessage("https://cdn.discordapp.com/attachments/568118396676341800/570348806437339144/1.gif");
-  } 
-    
-  if (message.content === ".lucy") {
-    message.channel.sendMessage("La meilleure amie de Milord et la future femme de Xamdark");
-    message.channel.sendMessage("https://cdn.discordapp.com/attachments/568118396676341800/570321500238774304/20190423_205336.gif");
-  } 
-    
-  if (message.content === ".kaisa") {
-    message.channel.sendMessage("Attaque trempette comme Carapuce :droplet: :whale:");
-    message.channel.sendMessage("https://tenor.com/view/k%c3%a9k%c3%a9crossing-carapuce-nyan-cat-pok%c3%a9mon-gif-13693447");
   } 
     
   if (message.content === ".chou") {
@@ -79,23 +90,17 @@ bot.on('message', message => {
     message.channel.sendMessage("https://tenor.com/view/supernatural-unicorn-rainbow-tail-gif-5754797");
   } 
     
-  if (message.content === ".golden") {
-    message.channel.sendMessage("Wallah c'est une victime");
-    message.channel.sendMessage("https://media.giphy.com/media/pPQyXGxwx272E/giphy.gif");
+  if (message.content === ".kaisa") {
+    message.channel.sendMessage("Attaque trempette comme Carapuce :droplet: :whale:");
+    message.channel.sendMessage("https://tenor.com/view/k%c3%a9k%c3%a9crossing-carapuce-nyan-cat-pok%c3%a9mon-gif-13693447");
   } 
     
-  if (message.content === ".yami") {
-    message.channel.sendMessage("Un homme qui ressent le besoin d etre superieur aux autres.");
-    message.channel.sendMessage("https://image.noelshack.com/fichiers/2019/17/1/1555965169-tumblr-pew9fkpgof1xqsds7o2-500.gif");
-  }
+  if (message.content === ".lucy") {
+    message.channel.sendMessage("La meilleure amie de Milord et la future femme de Xamdark");
+    message.channel.sendMessage("https://cdn.discordapp.com/attachments/568118396676341800/570321500238774304/20190423_205336.gif");
+  } 
 
-  if (message.content === ".loli") {
-    message.channel.sendMessage("https://image.noelshack.com/fichiers/2019/17/1/1555964709-tenor.gif");
-  }
 
-  if (message.content === ".anime") {
-    message.channel.sendMessage("https://www.randomanime.org/list/?l=703869fb&view=single&lang=any");
-  }   
     
    if (message.content === ".skribbl") {
     message.channel.sendMessage("https://skribbl.io/");
@@ -163,9 +168,9 @@ bot.on('message', message => {
   if (message.content === ".help1") {
     var help_embed = new Discord.RichEmbed()
       .setColor("#B638FF")
-      .addField("Les commandes inutiles" , ".yami.cora .illya .xam .golden .louhan .chou .kaisa.lucy .tezer.pute")
+      .addField("Les commandes inutiles" , ".yami.cora .illya .xam .golden .louhan .chou .kaisa .lucy .tezer")
     message.channel.sendEmbed(help_embed);
-    console.log("commande Help2 demandée !");
+    console.log("commande Help1 demandée !");
       
   }
 
@@ -173,9 +178,9 @@ bot.on('message', message => {
   if (message.content === ".help2") {
     var help_embed = new Discord.RichEmbed()
       .setColor("#B638FF")
-      .addField("Les commandes fun" , ".anime .dice .avatar .loli")
+      .addField("Les commandes utiles" , ".ping .ching .anime .dice .avatar .pute .loli .cheh")
     message.channel.sendEmbed(help_embed);
-    console.log("commande Help3 demandée !");
+    console.log("commande Help2 demandée !");
       
   }
 
