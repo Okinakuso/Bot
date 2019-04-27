e584458e23d6e97a77fca7400e1c8daae1cc6a64
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2019-04-27 12:40:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,8 +21,22 @@ function random(min, max) {
 
 
 bot.on('message', message => {
-  if (message.content.startsWith('.avatar ') && message.mentions.users.size) {
-    message.channel.sendMessage(message.author.avatarURL);
+  if (m.content.startsWith(".avatar")) {
+    if (m.content.indexOf(' ') !== -1) {
+      if (m.mentions) {
+        for (var user of m.mentions) {
+          message += user.avatarURL + '\n';
+        }
+        message.slice(0, -2);
+        bot.sendMessage(m.channel, message);
+      } else {
+        message = m.author.avatarURL;
+        bot.sendMessage(m.channel, message);
+      }
+    } else {
+      message = m.author.avatarURL;
+      bot.sendMessage(m.channel, message);
+    }
   }
     
  if (message.content === ".ping") {
