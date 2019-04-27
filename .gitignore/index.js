40a93ac175cb25f8cf96f00b6b474742f0997a85
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2019-04-27 20:36:31 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,156 +21,100 @@ function random(min, max) {
 
 
 bot.on('message', message => {
+
    if (message.content === ".avatar") {
     message.channel.send(message.author.avatarURL);
-  }
-    
-if (message.content.startsWith(`${prefix}ping`) {
-    message.channel.send("t as cru que j allais dire pong ou quoi ? Dechet !`);
-  }
-
-if (message.content.startsWith(`${prefix}ching") {
-    message.channel.send("tu es raciste !");
-  }    
-    
-if (message.content.startsWith(`${prefix}anime") {
-    message.channel.send("https://www.randomanime.org/list/?l=703869fb&view=single&lang=any");
-  }   
-    
-if (message.content.startsWith(`${prefix}pute") {
-    message.channel.send("INVOCATION !");
-    message.channel.send("http://image.noelshack.com/fichiers/2019/05/7/1549220487-dyvp0n-xqaajsne.jpg");
-    message.channel.send(message.author.avatarURL);
-  }
-
-if (message.content.startsWith(`${prefix}loli") {
-    message.channel.send("https://image.noelshack.com/fichiers/2019/17/1/1555964709-tenor.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}cheh`)) {
-	message.channel.send('https://thumbs.gfycat.com/AngryBrokenKodiakbear-max-1mb.gif');
    }
     
-if (message.content.startsWith(`${prefix}again") {
-    message.channel.send("come on");
-    message.channel.send("https://images-ext-2.discordapp.net/external/P2Ism1PGSkXCd7W_mEAzBoYvyBQIswXChL1RHmricow/%3Fitemid%3D13933485/https/media1.tenor.com/images/1bf906f1d7b433f8ed538f5ed00cb22e/tenor.gif");
-  }
- 
-if (message.content.startsWith(`${prefix}shit") {
-    message.channel.send("Tsukimasu kusotare");
-    message.channel.send("https://cdn.discordapp.com/attachments/568118396676341800/570625547319902208/ezgif.com-gif-maker.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}octogone") {
-    message.channel.send("Go octogone sans règles sous race !");
-    message.channel.send("https://66.media.tumblr.com/00adce6cac552d1126167f58628261bf/tumblr_pcrejxWhIh1usyygio2_400.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}fuck") {
-    message.channel.send("Just fuck off");
-    message.channel.send("https://media1.tenor.com/images/56ad2a999554be01ad74497c0d1325b7/tenor.gif?itemid=10678828");
-  }
-    
-if (message.content.startsWith(`${prefix}tera") {
-    message.channel.send("The definition of Tera");
-    message.channel.send("https://media.giphy.com/media/Y2mV4BtFdKy2E61CIU/giphy.gif");
-  }
-
-if (message.content.startsWith(`${prefix}slap") {
-    message.channel.send("Kuso !");
-    message.channel.send("https://media0.giphy.com/media/a7HKjDb3UJ0kM/giphy.gif");
-  }
-
-if (message.content.startsWith(`${prefix}cry") {
-    message.channel.send("no , i don't");
-    message.channel.send("http://66.media.tumblr.com/tumblr_m8ojbcznQ61rz5p5po3_500.gif");
-  }
-
-if (message.content.startsWith(`${prefix}hug") {
-    message.channel.send(":heart: :heart: :heart: :heart: :heart:");
-    message.channel.send("https://media1.tenor.com/images/efdd8f53689b1bb3437054d608156e95/tenor.gif?itemid=5101517");
-  }
-
-if (message.content.startsWith(`${prefix}yaoi") {
-    message.channel.send(":gay_pride_flag: :gay_pride_flag: ♂️♂️ :gay_pride_flag: :gay_pride_flag: ");
-    message.channel.send("https://media.giphy.com/media/7z1xs4Fl9Kb8A/giphy.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}yuri") {
-    message.channel.send(":gay_pride_flag: :gay_pride_flag: ♀️♀️ :gay_pride_flag: :gay_pride_flag: ");
-    message.channel.send("https://media.giphy.com/media/EVODaJHSXZGta/giphy.gif");
-  }
-
-if (message.content.startsWith(`${prefix}rly") {
-    message.channel.send("Ur in the shit ni****");
-    message.channel.send("https://i.kym-cdn.com/photos/images/original/000/420/723/b6b.gif");
+if (message.content.startsWith(`${prefix}ping`)) {
+    message.channel.send(`t as cru que j allais dire pong ou quoi ? Dechet !`);
+  } else if (message.content.startsWith(`${prefix}ching`)) {
+    message.channel.send(`tu es raciste !`);
+  }  else if (message.content.startsWith(`${prefix}anime`)) {
+    message.channel.send(`https://www.randomanime.org/list/?l=703869fb&view=single&lang=any`);
+  }  else if (message.content.startsWith(`${prefix}pute`)) {
+    message.channel.send(`INVOCATION !`);
+    message.channel.send(`http://image.noelshack.com/fichiers/2019/05/7/1549220487-dyvp0n-xqaajsne.jpg`);
+    message.channel.send(message.author.avatarURL);
+  }  else if (message.content.startsWith(`${prefix}loli`)) {
+    message.channel.send(`https://image.noelshack.com/fichiers/2019/17/1/1555964709-tenor.gif`);
+  }  else if (message.content.startsWith(`${prefix}shit`)) {
+   message.channel.send(`Tsukimasu kusotare`);
+   message.channel.send(`https://cdn.discordapp.com/attachments/568118396676341800/570625547319902208/ezgif.com-gif-maker.gif`);  
+  }  else if (message.content.startsWith(`${prefix}cheh`)) {
+    message.channel.send(`https://thumbs.gfycat.com/AngryBrokenKodiakbear-max-1mb.gif`);
+  }  else if (message.content.startsWith(`${prefix}octogone`)) {
+    message.channel.send(`Go octogone sans règles sous race !`);
+    message.channel.send(`https://66.media.tumblr.com/00adce6cac552d1126167f58628261bf/tumblr_pcrejxWhIh1usyygio2_400.gif`);
+  }  else if (message.content.startsWith(`${prefix}fuck`)) {
+    message.channel.send(`Just fuck off`);
+    message.channel.send(`https://media1.tenor.com/images/56ad2a999554be01ad74497c0d1325b7/tenor.gif?itemid=10678828`);
+  }  else if (message.content.startsWith(`${prefix}tera`)) {
+    message.channel.send(`The definition of Tera`);
+    message.channel.send(`https://media.giphy.com/media/Y2mV4BtFdKy2E61CIU/giphy.gif`);
+  }  else if (message.content.startsWith(`${prefix}slap`)) {
+    message.channel.send(`Kuso !`);
+    message.channel.send(`https://media0.giphy.com/media/a7HKjDb3UJ0kM/giphy.gif`);
+  }  else if (message.content.startsWith(`${prefix}cry`)) {
+    message.channel.send(`no , i don't`);
+    message.channel.send(`http://66.media.tumblr.com/tumblr_m8ojbcznQ61rz5p5po3_500.gif`);
+  }  else if (message.content.startsWith(`${prefix}hug`)) {
+    message.channel.send(`:heart: :heart: :heart: :heart: :heart:`);
+    message.channel.send(`https://media1.tenor.com/images/efdd8f53689b1bb3437054d608156e95/tenor.gif?itemid=5101517`);
+  }  else if (message.content.startsWith(`${prefix}yaoi`)) {
+    message.channel.send(`:gay_pride_flag: :gay_pride_flag: ♂️♂️ :gay_pride_flag: :gay_pride_flag: `);
+    message.channel.send(`https://media.giphy.com/media/7z1xs4Fl9Kb8A/giphy.gif`);
+  }  else if (message.content.startsWith(`${prefix}yuri`)) {
+    message.channel.send(`:gay_pride_flag: :gay_pride_flag: ♀️♀️ :gay_pride_flag: :gay_pride_flag: `);
+    message.channel.send(`https://media.giphy.com/media/EVODaJHSXZGta/giphy.gif`);
+  }  else if (message.content.startsWith(`${prefix}rly`)) {
+    message.channel.send(`Ur in the shit ni****`);
+    message.channel.send(`https://i.kym-cdn.com/photos/images/original/000/420/723/b6b.gif`);
   }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     
-if (message.content.startsWith(`${prefix}yami") {
-    message.channel.send("Un homme qui ressent le besoin d etre superieur aux autres.");
-    message.channel.send("https://image.noelshack.com/fichiers/2019/17/1/1555965169-tumblr-pew9fkpgof1xqsds7o2-500.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}cora") {
-    message.channel.send("La plus belle femme du monde et qui est aussi la futur femme de Yami");
-    message.channel.send("https://media.giphy.com/media/bm2O3nXTcKJeU/giphy.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}illya") {
-    message.channel.send("Le sac a foutre supreme mais aussi notre GM");
-    message.channel.send("https://media1.tenor.com/images/1e0593d9bf7b27cde0be27007d00887e/tenor.gif?itemid=5531465");
-  } 
-    
-if (message.content.startsWith(`${prefix}xam") {
-    message.channel.send("Faite attention a vous il est comme ca tout le temps");
-    message.channel.send("https://image.noelshack.com/fichiers/2019/17/1/1555964641-9aqn.gif");
-  } 
-    
-if (message.content.startsWith(`${prefix}golden") {
-    message.channel.send("Wallah c'est une victime");
-    message.channel.send("https://media.giphy.com/media/pPQyXGxwx272E/giphy.gif");
-  } 
-    
-if (message.content.startsWith(`${prefix}louhan") {
-    message.channel.send("Prenez garde à son futur 28M dps!");
-    message.channel.send("https://cdn.discordapp.com/attachments/568118396676341800/570343991292198932/20190423_222312.gif");
-  }
-    
-if (message.content.startsWith(`${prefix}tezer") {
-    message.channel.send("Paix et amour sont les mots qui le définissent!");
-    message.channel.send("https://cdn.discordapp.com/attachments/568118396676341800/570348806437339144/1.gif");
-  } 
-    
-if (message.content.startsWith(`${prefix}chou") {
-    message.channel.send("Je suis une licooooooorne :heart: * :heart:");
-    message.channel.send("https://tenor.com/view/supernatural-unicorn-rainbow-tail-gif-5754797");
-  } 
-    
-if (message.content.startsWith(`${prefix}kaisa") {
-    message.channel.send("Je t'aime, tu es parfait comme tu es. Ne change jamais c: :heart: * :heart: ");
-    message.channel.send("https://tenor.com/view/beer-unicorn-lets-get-drunk-gif-13996107");
-  } 
-    
-if (message.content.startsWith(`${prefix}lucy") {
-    message.channel.send("La meilleure amie de Milord et la future femme de Mme Su");
-    message.channel.send("https://cdn.discordapp.com/attachments/568118396676341800/570321500238774304/20190423_205336.gif");
+if (message.content.startsWith(`${prefix}yami`)) {
+    message.channel.send(`Un homme qui ressent le besoin d etre superieur aux autres.`);
+    message.channel.send(`https://image.noelshack.com/fichiers/2019/17/1/1555965169-tumblr-pew9fkpgof1xqsds7o2-500.gif`);
+  }  else if (message.content.startsWith(`${prefix}cora`)) {
+    message.channel.send(`La plus belle femme du monde et qui est aussi la futur femme de Yami`);
+    message.channel.send(`https://media.giphy.com/media/bm2O3nXTcKJeU/giphy.gif`);
+  }  else if (message.content.startsWith(`${prefix}illya`)) {
+    message.channel.send(`Le sac a foutre supreme mais aussi notre GM`);
+    message.channel.send(`https://media1.tenor.com/images/1e0593d9bf7b27cde0be27007d00887e/tenor.gif?itemid=5531465`);
+  }  else if (message.content.startsWith(`${prefix}xam`)) {
+    message.channel.send(`Faite attention a vous il est comme ca tout le temps`);
+    message.channel.send(`https://image.noelshack.com/fichiers/2019/17/1/1555964641-9aqn.gif`);
+  }  else if (message.content.startsWith(`${prefix}golden`)) {
+    message.channel.send(`Wallah c'est une victime`);
+    message.channel.send(`https://media.giphy.com/media/pPQyXGxwx272E/giphy.gif`);
+  }  else if (message.content.startsWith(`${prefix}louhan`)) {
+    message.channel.send(`Prenez garde à son futur 28M dps!`);
+    message.channel.send(`https://cdn.discordapp.com/attachments/568118396676341800/570343991292198932/20190423_222312.gif`);
+  }  else if (message.content.startsWith(`${prefix}tezer`)) {
+    message.channel.send(`Paix et amour sont les mots qui le définissent!`);
+    message.channel.send(`https://cdn.discordapp.com/attachments/568118396676341800/570348806437339144/1.gif`);
+  }  else if (message.content.startsWith(`${prefix}chou`)) {
+    message.channel.send(`Je suis une licooooooorne :heart: * :heart:`);
+    message.channel.send(`https://tenor.com/view/supernatural-unicorn-rainbow-tail-gif-5754797`);
+  }  else if (message.content.startsWith(`${prefix}kaisa`)) {
+    message.channel.send(`Je t'aime, tu es parfait comme tu es. Ne change jamais c: :heart: * :heart: `);
+    message.channel.send(`https://tenor.com/view/beer-unicorn-lets-get-drunk-gif-13996107`);
+  }  else if (message.content.startsWith(`${prefix}lucy`)) {
+    message.channel.send(`La meilleure amie de Milord et la future femme de Mme Su`);
+    message.channel.send(`https://cdn.discordapp.com/attachments/568118396676341800/570321500238774304/20190423_205336.gif`);
   } 
     
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-if (message.content.startsWith(`${prefix}skribbl") {
-    message.channel.send("https://skribbl.io/");
-  }   
-    
-if (message.content.startsWith(`${prefix}limit") {
-    message.channel.send("https://limitelimiteenligne.com/#/choose");
-  }   
-    
-if (message.content.startsWith(`${prefix}bac") {
-    message.channel.send("https://petitbacenligne.net/new-game.xhtml");
+if (message.content.startsWith(`${prefix}skribbl`)) {
+    message.channel.send(`https://skribbl.io/`);
+  }  else if (message.content.startsWith(`${prefix}limit`)) {
+    message.channel.send(`https://limitelimiteenligne.com/#/choose`);
+  }  else if (message.content.startsWith(`${prefix}bac`)) {
+    message.channel.send(`https://petitbacenligne.net/new-game.xhtml`);
   }               
     
   if (message.content === ".dice"){
