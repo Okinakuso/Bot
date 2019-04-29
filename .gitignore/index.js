4daf6cf61ae41040d91dc333d3ea551c4b95af1f
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2019-04-29 19:26:11 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -22,7 +22,9 @@ function random(min, max) {
 
 bot.on('message', message => {
 
-
+  if (message.content.startsWith(`${prefix}avatar`)) {
+    message.channel.send(message.author.avatarURL);
+   }
     
 if (message.content.startsWith(`${prefix}ping`)) {
     message.channel.send(`t as cru que j allais dire pong ou quoi ? Dechet !`);
@@ -45,10 +47,10 @@ if (message.content.startsWith(`${prefix}ping`)) {
   }  else if (message.content.startsWith(`${prefix}sauce`)) {
     message.channel.send(`https://cdn.discordapp.com/attachments/568118396676341800/572399987825049605/20190429_143256.gif`);
   }  else if (message.content.startsWith(`${prefix}jail`)) {
-    message.channel.send(`c est là où tu va aller.`);
+    message.channel.send(`c est là où tu va aller `);
     message.channel.send(`https://media1.giphy.com/media/l0HlxAqevo9mKD3UY/source.gif`);
   }  else if (message.content.startsWith(`${prefix}hungry`)) {
-    message.channel.send(`va chercher à manger au lieu d avoir la flemme dechet.`);
+    message.channel.send(`va chercher a manger au lieu d avoir la flemme dechet `);
   }  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
@@ -411,55 +413,7 @@ if (message.content.startsWith(`.pat`)) {
   }   
 
 }   
-if (message.content.startsWith(`.cat`)) {
-  random();   
 
-   if (randnum == 0){
-       message.channel.send(`http://25.media.tumblr.com/e60f80a75e70a30a9daeac0a80a6a7cc/tumblr_n1u8pk2OAM1trao0wo1_500.gif`);
-      console.log(randnum);
-    }  else if (randnum == 1){
-       message.channel.send(`https://static.fjcdn.com/gifs/Anime_ebb757_5470536.gif`);
-       console.log(randnum);
-     }  else if (randnum == 2){
-       message.channel.send(`http://24.media.tumblr.com/4f527f91cac82b39c1956c6e3f771555/tumblr_mqeregZSR21sv98gio1_500.gif`);
-       console.log(randnum);
-     }  else if (randnum == 3){
-        message.channel.send(`https://i.pinimg.com/originals/3a/46/19/3a4619e53f091dc9dd51d6c92d624b00.gif`);
-        console.log(randnum);
-     }  else if (randnum == 4){
-        message.channel.send(`https://media.giphy.com/media/wfBTUKEYpCcNi/giphy.gif`);
-        console.log(randnum);
-      }  else if (randnum == 5){
-        message.channel.send(`https://thumbs.gfycat.com/OrnerySlipperyGenet-size_restricted.gif`);
-        console.log(randnum);
- }    
-
-} 
-
-if (message.content.startWith(`.neko`)) {
-  random();
-
-   if (randnum == 0){
-     message.channel.send(`https://24.media.tumblr.com/47008dfaff99a408c8c16d71180757b1/tumblr_mpzvohOvNW1raqwzdo1_500.gif`);
-      console.log(randnum);
-      }  else if (randnum == 1){
-         message.channel.send(`https://i.kym-cdn.com/photos/images/original/000/936/930/bc4.gif`);
-     console.log(randnum);
-     }  else if (randnum == 2){
-     message.channel.send(`https://media1.giphy.com/media/UfZR37U3uqsTe/giphy.gif`);
-     console.log(randnum);
-     }  else if (randnum == 3){
-      message.channel.send(`http://emeraude1977.e.m.pic.centerblog.net/f5169382.gif`);
-      console.log(randnum);
-       }  else if (randnum == 4){
-      message.channel.send(`https://i.pinimg.com/originals/76/65/37/766537b607d97b8822ecf3d484b81ca3.gif`);
-        console.log(randnum);
-    }  else if (randnum == 5){
-    message.channel.send(`https://media1.tenor.com/images/37a585471f5e895489a03ae705430218/tenor.gif?itemid=12142150`);
-    console.log(randnum);
- }
- 
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
