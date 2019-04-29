1ef891f9bf71217c1bc042737c869051c2db33b3
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2019-04-29 20:10:20 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -415,33 +415,6 @@ if (message.content.startsWith(`.pat`)) {
 }   
 
 
-
-if (message.content.startWith(`.neko`)) {
-  random();
-
-   if (randnum == 0){
-       message.channel.send(`https://24.media.tumblr.com/47008dfaff99a408c8c16d71180757b1/tumblr_mpzvohOvNW1raqwzdo1_500.gif`);
-      console.log(randnum);
-     }  else if (randnum == 1){
-       message.channel.send(`https://i.kym-cdn.com/photos/images/original/000/936/930/bc4.gif`);
-      console.log(randnum);
-      } else if (randnum == 2){
-       message.channel.send(`https://media1.giphy.com/media/UfZR37U3uqsTe/giphy.gif`);
-       console.log(randnum);
-      } else if (randnum == 3){
-        message.channel.send(`http://emeraude1977.e.m.pic.centerblog.net/f5169382.gif`);
-       console.log(randnum);
-      } else if (randnum == 4){
-        message.channel.send(`https://i.pinimg.com/originals/76/65/37/766537b607d97b8822ecf3d484b81ca3.gif`);
-       console.log(randnum);
-     } else if (randnum == 5){
-       message.channel.send(`https://media1.tenor.com/images/37a585471f5e895489a03ae705430218/tenor.gif?itemid=12142150`);
-       console.log(randnum);
- }
- 
-}
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     
     
