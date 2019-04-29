c35cb335c5f58a8fb80e8fb2f13ab6b2a7bc1c89
Okinakuso <37508640+Okinakuso@users.noreply.github.com>
2019-04-29 20:15:36 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -413,6 +413,57 @@ if (message.content.startsWith(`.pat`)) {
  }   
 
 }   
+    
+if (message.content.startsWith(`.cat`)) {
+  random();   
+
+   if (randnum == 0){
+       message.channel.send(`http://25.media.tumblr.com/e60f80a75e70a30a9daeac0a80a6a7cc/tumblr_n1u8pk2OAM1trao0wo1_500.gif`);
+       console.log(randnum);
+      }  else if (randnum == 1){
+       message.channel.send(`https://static.fjcdn.com/gifs/Anime_ebb757_5470536.gif`);
+       console.log(randnum);
+     }  else if (randnum == 2){
+       message.channel.send(`http://24.media.tumblr.com/4f527f91cac82b39c1956c6e3f771555/tumblr_mqeregZSR21sv98gio1_500.gif`);
+       console.log(randnum);
+     }  else if (randnum == 3){
+        message.channel.send(`https://i.pinimg.com/originals/3a/46/19/3a4619e53f091dc9dd51d6c92d624b00.gif`);
+        console.log(randnum);
+     }  else if (randnum == 4){
+        message.channel.send(`https://media.giphy.com/media/wfBTUKEYpCcNi/giphy.gif`);
+        console.log(randnum);
+     }  else if (randnum == 5){
+        message.channel.send(`https://thumbs.gfycat.com/OrnerySlipperyGenet-size_restricted.gif`);
+        console.log(randnum);
+ }   
+
+}   
+
+
+if (message.content.startsWith(`.neko`)) {
+  random(); 
+
+  if (randnum == 0){
+       message.channel.send(`https://24.media.tumblr.com/47008dfaff99a408c8c16d71180757b1/tumblr_mpzvohOvNW1raqwzdo1_500.gif`);
+       console.log(randnum);
+      }  else if (randnum == 1){
+       message.channel.send(`https://i.kym-cdn.com/photos/images/original/000/936/930/bc4.gif`);
+       console.log(randnum);
+     }  else if (randnum == 2){
+       message.channel.send(`https://media1.giphy.com/media/UfZR37U3uqsTe/giphy.gif`);
+       console.log(randnum);
+     }  else if (randnum == 3){
+        message.channel.send(`http://emeraude1977.e.m.pic.centerblog.net/f5169382.gif`);
+        console.log(randnum);
+     }  else if (randnum == 4){
+        message.channel.send(`https://i.pinimg.com/originals/76/65/37/766537b607d97b8822ecf3d484b81ca3.gif`);
+        console.log(randnum);
+     }  else if (randnum == 5){
+       message.channel.send(`https://media1.tenor.com/images/37a585471f5e895489a03ae705430218/tenor.gif?itemid=12142150`);
+        console.log(randnum);
+ }   
+
+}   
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
